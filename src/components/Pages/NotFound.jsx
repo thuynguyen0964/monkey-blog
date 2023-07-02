fbc2a8d57d67572e0e9480ba4b429794f4470828
Thuy Nguyen <vanthuy101102@gmail.com>
2023-07-02 23:41:36 +0700

    Update UI Page not found, change Routing

--- src/components/Pages/NotFound.jsx
+++ src/components/Pages/NotFound.jsx
@@ -1,30 +1,62 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Button } from '../Button';
-
-const NotFoundStyles = styled.div`
-  position: relative;
-  .img-err {
-    width: 100%;
-    height: 100vh;
-    object-fit: cover;
+const PageNotFoundStyles = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.text};
+  color: white;
+  .page-content {
+    max-width: 1000px;
+    margin: 0 auto;
+    text-align: center;
   }
-  .child {
-    position: absolute;
-    bottom: 50px;
-    left: 50%;
-    transform: translate(-50%);
+  .logo {
+    display: inline-block;
+    margin-bottom: 40px;
+  }
+  .heading {
+    font-size: 60px;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+  .description {
+    max-width: 800px;
+    margin: 0 auto 40px;
+  }
+  .back {
+    display: inline-block;
+    padding: 15px 30px;
+    color: white;
+    background: ${(props) => props.theme.primary};
+    border-radius: 8px;
+    font-weight: 500;
+  }
+  .image {
+    max-width: 250px;
+    margin: 0 auto 40px;
   }
 `;
 
-const NotFound = () => {
+const PageNotFound = () => {
   return (
-    <NotFoundStyles>
-      <img src='/404-not-watermark.png' alt='' className='img-err' />
-      <div className='child'>
-        <Button to='/'>Home</Button>
+    <PageNotFoundStyles>
+      <div className='page-content'>
+        <img src='/src/assets/404.png' alt='notfound' className='image' />
+        <h1 className='heading'>404 - Looks like you are lost.</h1>
+        <p className='description'>
+          Maybe this page used to exist or you just spelled something wrong.
+          Chances are your spelled something wrong, so can you double check the
+          URL?
+        </p>
+        <Link to='/' className={'back'}>
+          Back to home
+        </Link>
       </div>
-    </NotFoundStyles>
+    </PageNotFoundStyles>
   );
 };
 
-export default NotFound;
+export default PageNotFound;
